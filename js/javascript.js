323d19c2b295c49f7b54f0ed76e86a45575704b9
YesidAros <yesid.aros@hotmail.com>
2025-02-22 23:16:38 -0500

    Diseño Header y responsive icono Hamburguesa

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menuToggle');
+    const menu = document.querySelector('.menu');
+
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+
+});
